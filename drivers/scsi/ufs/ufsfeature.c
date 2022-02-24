605005e5ac1c198d9f261fe8bfeaf28939f4eae5
Qilin Tan <qilin.tan@mediatek.com>
2022-02-24 14:30:06 +0800

    scsi:ufs: skip pm_runtime_get_sync when pm is in progress
    When the pm is in progress, the pm_runtime_get_sync should not be
    called. It will cause a dead lock.
    
    MTK-Commit-Id: f96f98fcb023640491192b71fae32206285c5538
    
    Signed-off-by: Qilin Tan <qilin.tan@mediatek.com>
    CR-Id: ALPS06711816
    Feature: ufs
    Change-Id: I5984580a89574c270b75fb6655bd8414c5725a1c

--- drivers/scsi/ufs/ufsfeature.c
+++ drivers/scsi/ufs/ufsfeature.c
@@ -189,8 +189,12 @@ static int ufsf_read_desc(struct ufs_hba *hba, u8 desc_id, u8 desc_index,
 			  u8 selector, u8 *desc_buf, u32 size)
 {
 	int err = 0;
+	bool pm_resumed = false;
 
-	pm_runtime_get_sync(hba->dev);
+	if (!hba->pm_op_in_progress) {
+		pm_runtime_get_sync(hba->dev);
+		pm_resumed = true;
+	}
 
 	err = ufshcd_query_descriptor_retry(hba, UPIU_QUERY_OPCODE_READ_DESC,
 					    desc_id, desc_index,
@@ -199,7 +203,8 @@ static int ufsf_read_desc(struct ufs_hba *hba, u8 desc_id, u8 desc_index,
 	if (err)
 		ERR_MSG("reading Device Desc failed. err = %d", err);
 
-	pm_runtime_put_sync(hba->dev);
+	if (pm_resumed)
+		pm_runtime_put_sync(hba->dev);
 
 	return err;
 }
